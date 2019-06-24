53255f308436c3ed69d032c0499bc88b32809623
cheetah747 <cheetah747@qq.com>
2019-06-24 16:49:50 +0800

    追加 スクショのタイムアウトは600に設定。

--- app/src/main/java/com/sibyl/sasukehomeDominator/MyAccessibilityService.kt
+++ app/src/main/java/com/sibyl/sasukehomeDominator/MyAccessibilityService.kt
@@ -25,7 +25,7 @@ class SasukeAccessibilityService : AccessibilityService() {
         //是否从通知栏瓷贴点击过来的（默认false）
 
         if (intent.getBooleanExtra(StaticVar.KEY_IS_FROM_SCRSHOT_TILE,false)){//是从瓷贴截屏按钮点击过来的
-            Handler().postDelayed({ performGlobalAction(GLOBAL_ACTION_TAKE_SCREENSHOT) },500)
+            Handler().postDelayed({ performGlobalAction(GLOBAL_ACTION_TAKE_SCREENSHOT) },600)
         }else{//常规长按HOME键过来的
             when (selected) {
                 StaticVar.LOCK_SCREEN -> performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)
