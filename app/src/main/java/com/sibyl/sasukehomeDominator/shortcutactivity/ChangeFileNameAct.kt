68a29d26183dafc60b2b0efd814e64b94073d3fc
cheetah747 <cheetah747@qq.com>
2019-06-28 19:54:29 +0800

    changeFileNameをスレッドに改善

--- app/src/main/java/com/sibyl/sasukehomeDominator/shortcutactivity/ChangeFileNameAct.kt
+++ app/src/main/java/com/sibyl/sasukehomeDominator/shortcutactivity/ChangeFileNameAct.kt
@@ -6,6 +6,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sibyl.sasukehomeDominator.util.PreferHelper
 import com.sibyl.sasukehomeDominator.util.StaticVar.Companion.KEY_SCREEN_SHOT_DIR
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import java.io.File
 import java.io.FileNotFoundException
 
@@ -17,22 +19,25 @@ class ChangeFileNameAct: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val scrShotDir = File(PreferHelper.getInstance().getString(KEY_SCREEN_SHOT_DIR,
-                        Environment.getExternalStorageDirectory().path +"/Pictures/Screenshots/"))
-        try {
-            var count = 0
-            scrShotDir.listFiles { dir, name ->
-                name.endsWith(".png")
-            }.forEach {
-                count++
-                it.renameTo(File(it.path?.replace(".png",".jpg")))
+        doAsync {
+            val scrShotDir = File(PreferHelper.getInstance().getString(KEY_SCREEN_SHOT_DIR,
+                Environment.getExternalStorageDirectory().path +"/Pictures/Screenshots/"))
+            try {
+                var count = 0
+                scrShotDir.listFiles { dir, name ->
+                    name.endsWith(".png")
+                }.forEach {
+                    count++
+                    it.renameTo(File(it.path?.replace(".png",".jpg")))
+                }
+                uiThread { Toast.makeText(this@ChangeFileNameAct,if (count == 0) "変換するファイルはありません"
+                                            else "${count} 個ファイル変換完了" ,Toast.LENGTH_LONG).show() }
+            }catch (e: FileNotFoundException){
+                e.printStackTrace()
+                uiThread { Toast.makeText(this@ChangeFileNameAct,e.toString(),Toast.LENGTH_LONG).show() }
             }
-            Toast.makeText(this,if (count == 0) "変換するファイルはありません"
-                                                                else "${count} 個ファイル変換完了" ,Toast.LENGTH_LONG).show()
-        }catch (e: FileNotFoundException){
-            e.printStackTrace()
-            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show()
+            finish()
         }
-        finish()
+
     }
 }
