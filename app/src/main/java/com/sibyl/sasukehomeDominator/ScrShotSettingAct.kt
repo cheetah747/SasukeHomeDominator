8407b5fd1625ed74680cb4abc8b971d8eb58f433
cheetah747 <cheetah747@qq.com>
2019-09-29 17:59:04 +0800

    为了对策安卓10的监听不生效问题，新增手动选择截屏路径弹窗。

--- app/src/main/java/com/sibyl/sasukehomeDominator/ScrShotSettingAct.kt
+++ app/src/main/java/com/sibyl/sasukehomeDominator/ScrShotSettingAct.kt
@@ -1,13 +1,16 @@
 package com.sibyl.sasukehomeDominator
 
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatCheckBox
 import androidx.appcompat.widget.Toolbar
+import com.sibyl.sasukehomeDominator.util.FolderSelectorDialog
 import com.sibyl.sasukehomeDominator.util.PreferHelper
 import com.sibyl.sasukehomeDominator.util.StaticVar
+import com.sibyl.sasukehomeDominator.util.StaticVar.Companion.KEY_SCREEN_SHOT_DIR
 import kotlinx.android.synthetic.main.screen_shot_setting_act.*
 import org.jetbrains.anko.find
 
@@ -18,7 +21,7 @@ import org.jetbrains.anko.find
 class ScrShotSettingAct : AppCompatActivity() {
     companion object {
         const val SECONDS_0 = 0
-        const val SECONDS_3 = 3000
+        const val SECONDS_2 = 2000
         const val SECONDS_5 = 5000
 
     }
@@ -62,16 +65,22 @@ class ScrShotSettingAct : AppCompatActivity() {
                 //保存水印
                 setBoolean(StaticVar.KEY_IS_SHOW_WATERMARK, waterMarkCheck.isChecked)
                 //保存用户名
-                setStringCommit(StaticVar.KEY_USER_NAME,userName.text.toString().trim().run { if (isNotBlank()) this else "Android ${android.os.Build.VERSION.RELEASE}"  } )
+                setStringCommit(
+                    StaticVar.KEY_USER_NAME,
+                    userName.text.toString().trim().run { if (isNotBlank()) this else "Android ${android.os.Build.VERSION.RELEASE}" })
                 //保存手机型号
-                setStringCommit(StaticVar.KEY_PHONE_MODEL,phoneInfo.text.toString().trim().run { if (isNotBlank()) this else android.os.Build.MODEL  })
+                setStringCommit(
+                    StaticVar.KEY_PHONE_MODEL,
+                    phoneInfo.text.toString().trim().run { if (isNotBlank()) this else android.os.Build.MODEL })
                 //保存截屏延迟
-                setIntCommit(StaticVar.KEY_TIME_TO_SCRSHOT,when(true){
-                    seconds0.isChecked -> SECONDS_0
-                    seconds3.isChecked -> SECONDS_3
-                    seconds5.isChecked -> SECONDS_5
-                    else -> SECONDS_0
-                })
+                setIntCommit(
+                    StaticVar.KEY_TIME_TO_SCRSHOT, when (true) {
+                        seconds0.isChecked -> SECONDS_0
+                        seconds3.isChecked -> SECONDS_2
+                        seconds5.isChecked -> SECONDS_5
+                        else -> SECONDS_0
+                    }
+                )
             }
             Toast.makeText(this, "設定が保存されました", Toast.LENGTH_SHORT).show()
             onBackPressed()
@@ -84,7 +93,8 @@ class ScrShotSettingAct : AppCompatActivity() {
         userName.hint = "Android ${android.os.Build.VERSION.RELEASE}"
         PreferHelper.getInstance().getString(StaticVar.KEY_USER_NAME, "").run {
             userName.setText(if (isNotBlank()) this else "Android ${android.os.Build.VERSION.RELEASE}".apply {
-                PreferHelper.getInstance().setStringCommit(StaticVar.KEY_USER_NAME, "Android ${android.os.Build.VERSION.RELEASE}")
+                PreferHelper.getInstance()
+                    .setStringCommit(StaticVar.KEY_USER_NAME, "Android ${android.os.Build.VERSION.RELEASE}")
             })
         }
         //手机型号
@@ -101,13 +111,26 @@ class ScrShotSettingAct : AppCompatActivity() {
         refreshSecondsChecks(
             when (PreferHelper.getInstance().getInt(StaticVar.KEY_TIME_TO_SCRSHOT, 0)) {
                 SECONDS_0 -> seconds0
-                SECONDS_3 -> seconds3
+                SECONDS_2 -> seconds3
                 SECONDS_5 -> seconds5
                 else -> seconds0
             }
         )
 
 
+        //针对安卓10，新增手动指定截屏路径（安卓10老子操你妈。。。。）
+        dirSelectLayout.visibility = if (android.os.Build.VERSION.RELEASE.toDouble() < 10) View.GONE else View.VISIBLE
+        dirSelectTv.text = PreferHelper.getInstance().getString(
+            KEY_SCREEN_SHOT_DIR,
+            Environment.getExternalStorageDirectory().path + "/Pictures/Screenshots/"
+        )
+        //用户自行修改
+        dirSelectTv.setOnClickListener {
+            FolderSelectorDialog.showDialog(this) { file ->
+                dirSelectTv.text = file.canonicalPath
+                PreferHelper.getInstance().setString(KEY_SCREEN_SHOT_DIR, file.canonicalPath)
+            }
+        }
     }
 
     /**刷新秒数选择的单选框状态*/
---
+++ app/src/main/java/com/sibyl/sasukehomeDominator/util/FolderSelectorDialog.kt
@@ -0,0 +1,70 @@
+package com.sibyl.sasukehomeDominator.util
+
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Environment
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import com.sibyl.sasukehomeDominator.R
+import org.jetbrains.anko.find
+import java.io.File
+
+/**
+ * @author Sasuke on 2019-9-29 0029.
+ * 紧急加的一个破烂选择路径弹窗
+ */
+class FolderSelectorDialog {
+    companion object {
+        @JvmStatic
+        fun showDialog(context: Context, okCallback: (File) -> Unit) {
+            val view = LayoutInflater.from(context).inflate(R.layout.folder_selector_layout, null)
+            val listView = view.find<ListView>(R.id.fileLv)
+            val goPre = view.find<TextView>(R.id.goPre)
+            val currDirTv = view.find<TextView>(R.id.currDirTv)
+            //先读取路径记录
+            var currFileDir = File(
+                PreferHelper.getInstance().getString(
+                    StaticVar.KEY_SCREEN_SHOT_DIR,
+                    Environment.getExternalStorageDirectory().path + "/Pictures/Screenshots/"
+                )
+            )
+            //显示当前位置标题：
+            currDirTv.text = "ディレクトリ：${currFileDir.canonicalPath}"
+
+            //再填充列表
+            listView.adapter = ArrayAdapter(
+                context,
+                android.R.layout.simple_expandable_list_item_1,
+                currFileDir.list { file, str -> file.isDirectory })
+
+            //点击监听
+            listView.setOnItemClickListener { adapterView, view, pos, id ->
+                currFileDir = currFileDir.listFiles { file, str -> file.isDirectory }[pos]
+                currDirTv.text = "ディレクトリ：${currFileDir.canonicalPath}"
+                listView.adapter = ArrayAdapter(//妈的懒得抽出来了，直接复制粘贴，烦死了
+                    context,
+                    android.R.layout.simple_expandable_list_item_1,
+                    currFileDir.list { file, str -> file.isDirectory })
+            }
+            //上一级
+            goPre.setOnClickListener {
+                currFileDir = currFileDir.parentFile
+                currDirTv.text = "ディレクトリ：${currFileDir.canonicalPath}"
+                listView.adapter = ArrayAdapter(//妈的懒得抽出来了，直接复制粘贴，烦死了
+                    context,
+                    android.R.layout.simple_expandable_list_item_1,
+                    currFileDir.list { file, str -> file.isDirectory })
+            }
+
+            androidx.appcompat.app.AlertDialog.Builder(context)
+                .setView(view)
+                .setPositiveButton("OK", DialogInterface.OnClickListener { dialogInterface, i ->
+                    okCallback.invoke(currFileDir)
+                }).show()
+        }
+    }
+
+}
+
