e9edcbaf2e20315f0ae76a3fc4befb94e3c83943
cheetah747 <cheetah747@qq.com>
2019-11-24 15:43:41 +0800

    ナビゲーションバーを色づける、

--- app/src/main/java/com/sibyl/sasukehomeDominator/util/BaseActivity.kt
+++ app/src/main/java/com/sibyl/sasukehomeDominator/util/BaseActivity.kt
@@ -2,8 +2,10 @@ package com.sibyl.sasukehomeDominator.util
 
 import android.content.Context
 import android.content.res.Configuration
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.sibyl.sasukehomeDominator.R
 
 
 /**
@@ -13,6 +15,12 @@ open class BaseActivity: AppCompatActivity() {
     override fun attachBaseContext(newBase: Context) {
         //Public.Language 为 对应的资源格式后缀，比如"zh"      "
         super.attachBaseContext(MyContextWrapper.wrap(newBase, ""))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //着色导航栏
+        getWindow().setNavigationBarColor(resources.getColor(R.color.white,null))
     }
 
     //适配黑暗模式 https://www.jianshu.com/p/ba62cbcc9075
